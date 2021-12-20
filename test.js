c58356317e45ac3ec7f1c1ed4f108e4cdb75eb1c
Invidam <hansupark0208@gmail.com>
2021-12-20 17:44:27 +0900

    12_20 update

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-// Prettier 적용 위한 파일
+function getArgs() {
+  console.log(arguments);
+  const slice = Array.prototype.slice;
+  console.log(slice);
+  const arr = slice.call(arguments);
+  console.log(arr, this);
+  return arr;
+}
+
+console.log(getArgs(1, 2, 3));
