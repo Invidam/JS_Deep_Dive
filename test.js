2a4df442e9f5de01492b7ee839919b126eaae091
Invidam <hansupark0208@gmail.com>
2021-12-23 11:18:38 +0900

    오타 수정

--- test.js
+++ test.js
@@ -1,28 +1,25 @@
 // function Person(name, age) {
 //   this.name = name;
-//   let _age = age;
 
 //   this.sayHi = function () {
-//     console.log(`Hi I am ${this.name}(${_age}) `);
+//     console.log(`Hi I am ${this.name}`);
 //   };
 // }
-const Person = (function () {
-  let _age;
-  function Person(name, age) {
+
+// const me = new Person("Park", 22);
+// me.sayHi();
+// console.log(me.name, me._age);
+
+class Person {
+  constructor(name = "person") {
     this.name = name;
-    _age = age;
   }
-  Person.prototype.sayHi = function () {
-    console.log(`Hi I am ${this.name}(${_age}) `);
-  };
-  return Person;
-})();
-const me = new Person("Park", 22);
-me.sayHi();
-console.log(me.name, me._age); //Park , undefined
-
-const you = new Person("Lee", 27);
-you.sayHi();
-console.log(you.name, you._age); //Park , undefined
-
+  sayHi() {
+    console.log(`Hi I am ${this.name}`);
+  }
+  static sayHello() {
+    console.log("HI");
+  }
+}
+const me = new Person();
 me.sayHi();
