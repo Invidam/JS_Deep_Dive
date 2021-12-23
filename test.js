355268317ddc191a34272ae23f061cd8eb136104
Invidam <hansupark0208@gmail.com>
2021-12-23 23:51:14 +0900

    25_클래스 정리  완료

--- test.js
+++ test.js
@@ -1,25 +1,47 @@
-// function Person(name, age) {
-//   this.name = name;
-
-//   this.sayHi = function () {
-//     console.log(`Hi I am ${this.name}`);
-//   };
-// }
-
-// const me = new Person("Park", 22);
-// me.sayHi();
-// console.log(me.name, me._age);
-
-class Person {
-  constructor(name = "person") {
-    this.name = name;
+class Animal {
+  constructor(age, weight) {
+    this.age = age;
+    this.weight = weight;
   }
-  sayHi() {
-    console.log(`Hi I am ${this.name}`);
+  testValue = 123;
+  eat() {
+    return "eat";
   }
-  static sayHello() {
-    console.log("HI");
+  move() {
+    return "move";
+  }
+  info() {
+    return this.age + ", " + this.weight;
   }
 }
-const me = new Person();
-me.sayHi();
+
+class Bird extends Animal {
+  fly() {
+    return "fly";
+  }
+}
+const ani = new Animal(20, 30);
+const bird = new Bird(5, 3);
+const bird2 = new Bird(6, 30);
+
+// console.log(bird.eat());
+// console.log(bird.fly());
+// console.log(bird.info());
+
+// console.log(bird.info());
+// console.log(bird2.info());
+
+console.log(ani.testValue);
+console.log(bird.testValue);
+console.dir(ani);
+console.dir(bird);
+
+Bird.testValue = 321;
+
+console.log(ani.testValue);
+console.log(bird.testValue);
+
+console.dir(ani);
+console.dir(bird);
+
+constructor(a,b,c) {super(a,b); this.c = c}
